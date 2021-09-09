06b9f29dc7afba7e0e882492cd1d543a34c08615
tanka raj bhattarai <51885260+tanka76@users.noreply.github.com>
2021-09-09 11:40:06 +0545

    Update settings.py

--- veremail/src/settings.py
+++ veremail/src/settings.py
@@ -125,6 +125,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bhattaraisapana525@gmail.com'
-EMAIL_HOST_PASSWORD = 'sapana1818'
+EMAIL_HOST_USER = 'xyx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xyz'
 
